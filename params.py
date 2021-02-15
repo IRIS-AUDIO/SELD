1f93aaf68065f40c4aa63af567a1312e22b28b25
Seungjin Lee <lsg0385@naver.com>
2021-02-15 01:25:48 +0000

    [add] several version of initial seldnet and attention seldnet

--- params.py
+++ params.py
@@ -14,16 +14,21 @@ def get_param(known=None):
     args.add_argument('--config_mode', type=str, default='')
     args.add_argument('--doa_loss', type=str, default='MSE', 
                       choices=['MAE', 'MSE', 'MSLE'])
+    args.add_argument('--model', type=str, default='initial_seldnet_v1', 
+                      choices=['initial_seldnet', 'initial_seldnet_v1', 'initial_seldnet_v2', 'initial_seldnet_v3', 'attention_seldnet'])
+    
     
     # training
     args.add_argument('--lr', type=float, default=0.001)
     args.add_argument('--decay', type=float, default=1/np.sqrt(2))
     args.add_argument('--batch', type=int, default=256)
-    args.add_argument('--epoch', type=int, default=50)
+    args.add_argument('--epoch', type=int, default=200)
     args.add_argument('--loss_weight', type=str, default='1,1000')
-    args.add_argument('--patience', type=int, default=10)
+    args.add_argument('--patience', type=int, default=100)
     args.add_argument('--freq_mask_size', type=int, default=8)
     args.add_argument('--time_mask_size', type=int, default=24)
+    args.add_argument('--maxstep', type=int, default=50)
+    args.add_argument('--inf', action='store_true')
 
     # metric
     args.add_argument('--lad_doa_thresh', type=int, default=20)
@@ -31,6 +36,7 @@ def get_param(known=None):
     if known is None:
         known = []
     config = args.parse_known_args(known)[0]
+    config.name = config.model + '_' + config.name
     config = get_config(config.name, config, mode=config.config_mode)
 
     return config
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 from data_loader import *
 from metrics import evaluation_metrics, SELD_evaluation_metrics
-from models import build_seldnet
+import models
 from params import get_param
 from transforms import *
 
@@ -51,7 +51,7 @@ def metric(metric_class, preds, gts, class_num):
     return test_new_metric, test_new_seld_metric
 
 
-def iterloop(model, dataset, sed_loss, doa_loss, metric_class, config, class_num, epoch, writer, optimizer=None, mode='train'):
+def iterloop(model, dataset, sed_loss, doa_loss, metric_class, config, class_num, epoch, writer, maxstep=0, optimizer=None, mode='train'):
     # metric
     ER = tf.keras.metrics.Mean()
     F = tf.keras.metrics.Mean()
@@ -60,11 +60,15 @@ def iterloop(model, dataset, sed_loss, doa_loss, metric_class, config, class_num
     SeldScore = tf.keras.metrics.Mean()
     ssloss = tf.keras.metrics.Mean()
     ddloss = tf.keras.metrics.Mean()
+    if maxstep == 0:
+        maxstep = len(dataset)
 
     loss_weight = [int(i) for i in config.loss_weight.split(',')]
-    with tqdm(dataset) as pbar:
-        for x, y in pbar:
+    with tqdm(dataset, total=maxstep) as pbar:
+        for step, (x, y) in enumerate(pbar):
             if mode == 'train':
+                if step == maxstep:
+                    break
                 preds, sloss, dloss = trainstep(model, x, y, sed_loss, doa_loss, loss_weight, optimizer)
             else:
                 preds, sloss, dloss = teststep(model, x, y, sed_loss, doa_loss)
@@ -105,12 +109,13 @@ def get_dataset(config, mode:str='train'):
     batch_transforms = [
         split_total_labels_to_sed_doa
     ]
-    dataset = seldnet_data_to_dataloader( # 나중에 batch 조절 가능하도록 수정
+    dataset = seldnet_data_to_dataloader(
         x, y,
         sample_transforms=sample_transforms,
         batch_transforms=batch_transforms,
         label_window_size=time_length,
-        batch_size=config.batch
+        batch_size=config.batch,
+        inf_loop=True if config.inf and mode == 'train' else False
     )
     return dataset
 
@@ -144,8 +149,8 @@ def main(config):
     del a
 
     # model load
-    model = build_seldnet(input_shape, n_classes=class_num)
-
+    model = getattr(models, config.model)(input_shape, n_classes=class_num)
+    
     optimizer = tf.keras.optimizers.Adam(learning_rate=config.lr)
     sed_loss = tf.keras.losses.BinaryCrossentropy(name='sed_loss')
     doa_loss = getattr(tf.keras.losses, config.doa_loss)
@@ -166,7 +171,7 @@ def main(config):
     for epoch in range(config.epoch):
         # train loop
         metric_class.reset()
-        iterloop(model, trainset, sed_loss, doa_loss, metric_class, config, class_num, epoch, writer, optimizer=optimizer, mode='train') 
+        iterloop(model, trainset, sed_loss, doa_loss, metric_class, config, class_num, epoch, writer, config.maxstep, optimizer=optimizer, mode='train') 
 
         # validation loop
         metric_class.reset()
@@ -180,7 +185,7 @@ def main(config):
             os.system(f'rm -rf {model_path}/bestscore_{best_score}.hdf5')
             best_score = score
             patience = 0
-            tf.keras.models.save_model(model, os.path.join(model_path, f'bestscore_{best_score}.hdf5'))
+            tf.keras.models.save_model(model, os.path.join(model_path, f'bestscore_{best_score}.hdf5'), include_optimizer=False)
         else:
             if patience == config.patience:
                 print(f'Early Stopping at {epoch}, score is {score}')
