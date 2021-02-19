2a0db34bac0078991299981caaabd98299f90f4a
daniel03c1 <daniel03c195@gmail.com>
2021-02-19 11:11:55 +0000

    [fix] models.py fixed (initial_seldnet)

--- models.py
+++ models.py
@@ -14,7 +14,8 @@ def initial_seldnet(data_in,
     data_in: [batch, freq, time, chan]
     '''
     if pool_size is None:
-        pool_size = [8, 8, 2]
+        pool_size = [4, 4, 2]
+    t_pool_size = [5, 1, 1]
     if rnn_size is None:
         rnn_size = [128, 128]
     if fnn_size is None:
@@ -28,11 +29,10 @@ def initial_seldnet(data_in,
         spec_cnn = Conv2D(nb_cnn2d_filt, kernel_size=3, padding='same')(spec_cnn)
         spec_cnn = BatchNormalization()(spec_cnn)
         spec_cnn = Activation('relu')(spec_cnn)
-        spec_cnn = MaxPooling2D(pool_size=(pool_size[i], 1))(spec_cnn)
+        spec_cnn = MaxPooling2D(pool_size=(t_pool_size[i], pool_size[i]))(spec_cnn)
         spec_cnn = Dropout(dropout_rate)(spec_cnn)
 
-    # [b, f, t, c] -> [b, t, c]
-    spec_cnn = Permute((2, 1, 3))(spec_cnn)
+    # [b, t, f, c] -> [b, t, c]
     spec_rnn = Reshape((-1, spec_cnn.shape[-2]*spec_cnn.shape[-1]))(spec_cnn)
 
     for nb_rnn_filt in rnn_size:
