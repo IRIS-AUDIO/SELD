14d90de81d38b6a3e12124bb21f7e509df9705a5
daniel03c1 <daniel03c195@gmail.com>
2021-02-25 13:56:07 +0000

    [update] updated models_test.py

--- models_test.py
+++ models_test.py
@@ -13,15 +13,18 @@ class ModelsTest(tf.test.TestCase):
 
         self.x = tf.zeros((self.batch, self.freq, self.time, self.chan))
 
-    def test_build_seldnet(self):
-        model = build_seldnet([self.freq, self.time, self.chan],
-                              self.n_classes)
+    def test_seldnet_architecture(self):
+        model = seldnet_architecture(
+            self.x.shape,
+            hlfr=lambda x: x,
+            tcr=lambda x: tf.reduce_mean(x, axis=1),
+            sed=lambda x: tf.keras.layers.Dense(self.n_classes)(x),
+            doa=lambda x: tf.keras.layers.Dense(self.n_classes*3)(x))
         y = model.predict(self.x)
 
         # SED
         self.assertAllEqual(y[0].shape, 
                             [self.batch, self.time, self.n_classes])
-        
         # DOA
         self.assertAllEqual(y[1].shape, 
                             [self.batch, self.time, self.n_classes*3])
