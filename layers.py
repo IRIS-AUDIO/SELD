96a703967db1a05db6d317b5d3f46be6ad7ef728
daniel03c1 <daniel03c195@gmail.com>
2021-03-08 03:31:42 +0000

    Updated layers (removed old layers and added kernel regularizer)

--- layers.py
+++ layers.py
@@ -3,77 +3,14 @@ import tensorflow.keras as keras
 from tensorflow.keras.layers import *
 
 
-def high_level_feature_representation_base(config):
-    t_pool_size = [int(i) for i in config.high_level_feature_representation_pool1.split(',')]
-    pool_size = [int(i) for i in config.high_level_feature_representation_pool2.split(',')]
-    
-    def _high_level_feature_representation(inputs):
-        x = inputs
-        for i, convCnt in enumerate(pool_size):
-            x = Conv2D(config.high_level_feature_representation_filter, kernel_size=3, padding='same')(
-                x)
-            x = BatchNormalization()(x)
-            x = Activation('relu')(x)
-            x = MaxPooling2D(pool_size=(t_pool_size[i], pool_size[i]))(
-                x)
-            x = Dropout(config.dropout_rate)(x)
-        outputs = x
-        return outputs
-    return _high_level_feature_representation
-
-
-def temporal_context_representation_base(config):
-    rnn_size = [int(i) for i in config.temporal_context_representation_filter.split(',')]
-
-    def _temporal_context_representation(inputs):
-        x = inputs
-        # [b, t, f, c] -> [b, t, c]
-        x = Reshape((-1, x.shape[-2]*x.shape[-1]))(x)
-        for nb_rnn_filt in rnn_size:
-            x = Bidirectional(
-                GRU(nb_rnn_filt, activation='tanh', 
-                    dropout=config.dropout_rate, recurrent_dropout=config.dropout_rate, 
-                    return_sequences=True),
-                merge_mode='mul')(x)
-        outputs = x
-        return outputs
-    return _temporal_context_representation
-
-
-def sed_layer_base(config, n_classes):
-    fnn_size = [int(i) for i in config.sed_layer_filter.split(',')]
-    
-    def _sed_layer(inputs):
-        x = inputs
-        for nb_fnn_filt in fnn_size:
-            x = TimeDistributed(Dense(nb_fnn_filt))(x)
-            x = Dropout(config.dropout_rate)(x)
-        x = TimeDistributed(Dense(n_classes, name='sed_out'))(x)
-        outputs = x
-        return outputs
-    return _sed_layer
-
-
-def doa_layer_base(config, n_classes):
-    fnn_size = [int(i) for i in config.doa_layer_filter.split(',')]
-
-    def _doa_layer(inputs):
-        x = inputs
-        for nb_fnn_filt in fnn_size:
-            x = TimeDistributed(Dense(nb_fnn_filt))(x)
-            x = Dropout(config.dropout_rate)(x)
-        x = TimeDistributed(Dense(n_classes*3, name='doa_out'))(x) 
-        outputs = x
-        return outputs
-    return _doa_layer
-
-
 def simple_conv_block(model_config: dict):
     # mandatory parameters
     filters = model_config['filters']
     pool_size = model_config['pool_size']
 
     dropout_rate = model_config.get('dropout_rate', 0.)
+    kernel_regularizer = tf.keras.regularizers.l1_l2(
+        **model_config.get('kernel_regularizer', {'l1': 0., 'l2': 0.}))
 
     if len(filters) == 0:
         filters = filters * len(pool_size)
@@ -83,7 +20,8 @@ def simple_conv_block(model_config: dict):
     def conv_block(inputs):
         x = inputs
         for i in range(len(filters)):
-            x = Conv2D(filters[i], kernel_size=3, padding='same')(x)
+            x = Conv2D(filters[i], kernel_size=3, padding='same', 
+                       kernel_regularizer=kernel_regularizer)(x)
             x = BatchNormalization()(x)
             x = Activation('relu')(x)
             x = MaxPooling2D(pool_size=pool_size[i])(x)
@@ -120,17 +58,22 @@ def simple_dense_block(model_config: dict):
     units_per_layer = model_config['units']
     n_classes = model_config['n_classes']
 
-    dropout_rate = model_config.get('dropout_rate', 0)
-    activation = model_config.get('activation', None)
     name = model_config.get('name', None)
+    activation = model_config.get('activation', None)
+    dropout_rate = model_config.get('dropout_rate', 0)
+    kernel_regularizer = tf.keras.regularizers.l1_l2(
+        **model_config.get('kernel_regularizer', {'l1': 0., 'l2': 0.}))
 
     def dense_block(inputs):
         x = inputs
         for units in units_per_layer:
-            x = TimeDistributed(Dense(units))(x)
+            x = TimeDistributed(
+                Dense(units, kernel_regularizer=kernel_regularizer))(x)
             x = Dropout(dropout_rate)(x)
         x = TimeDistributed(
-            Dense(n_classes, activation=activation, name=name))(x) 
+            Dense(n_classes, activation=activation, name=name,
+                  kernel_regularizer=kernel_regularizer))(x) 
         return x
 
     return dense_block
+
