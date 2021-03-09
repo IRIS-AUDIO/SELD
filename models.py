1907eca27dc44f6126ba013b7d190574e528a238
daniel03c1 <daniel03c195@gmail.com>
2021-03-09 02:44:49 +0000

    seldnet_architecture was removed and the description of models.py was added. TODO: tests are required

--- models.py
+++ models.py
@@ -2,17 +2,24 @@ import tensorflow as tf
 from tensorflow.keras.activations import *
 from tensorflow.keras.layers import *
 
-import layers
+import modules
+
+"""
+Models
+
+This defines model structures (architectures)
+You should not define modules nor layers here.
+"""
 
 
 def seldnet(input_shape, model_config):
     # interprets model_config to an actual model
     inputs = Input(shape=input_shape[-3:])
 
-    x = getattr(layers, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
-    x = getattr(layers, model_config.SECOND)(model_config.SECOND_ARGS)(x)
-    sed = getattr(layers, model_config.SED)(model_config.SED_ARGS)(x)
-    doa = getattr(layers, model_config.DOA)(model_config.DOA_ARGS)(x)
+    x = getattr(modules, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
+    x = getattr(modules, model_config.SECOND)(model_config.SECOND_ARGS)(x)
+    sed = getattr(modules, model_config.SED)(model_config.SED_ARGS)(x)
+    doa = getattr(modules, model_config.DOA)(model_config.DOA_ARGS)(x)
 
     return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
 
@@ -20,10 +27,10 @@ def seldnet(input_shape, model_config):
 def seldnet_v1(input_shape, model_config):
     inputs = Input(shape=input_shape[-3:])
 
-    x = getattr(layers, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
-    x = getattr(layers, model_config.SECOND)(model_config.SECOND_ARGS)(x)
-    sed = getattr(layers, model_config.SED)(model_config.SED_ARGS)(x)
-    doa = getattr(layers, model_config.DOA)(model_config.DOA_ARGS)(x)
+    x = getattr(modules, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
+    x = getattr(modules, model_config.SECOND)(model_config.SECOND_ARGS)(x)
+    sed = getattr(modules, model_config.SED)(model_config.SED_ARGS)(x)
+    doa = getattr(modules, model_config.DOA)(model_config.DOA_ARGS)(x)
 
     doa *= Concatenate()([sed] * 3)
     doa = tanh(doa) 
@@ -31,24 +38,14 @@ def seldnet_v1(input_shape, model_config):
     return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
 
 
-def seldnet_architecture(input_shape, model_config):
-    # interprets model_config to an actual model
-    inputs = Input(shape=input_shape[-3:])
-
-    x = getattr(layers, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
-    x = getattr(layers, model_config.SECOND)(model_config.SECOND_ARGS)(x)
-    sed = getattr(layers, model_config.SED)(model_config.SED_ARGS)(x)
-    doa = getattr(layers, model_config.DOA)(model_config.DOA_ARGS)(x)
-
-    return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
-
 def xception_gru(input_shape, model_config):
     # interprets model_config to an actual model
     inputs = Input(shape=input_shape[-3:])
 
-    x = getattr(layers, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
-    x = getattr(layers, model_config.SECOND)(model_config.SECOND_ARGS)(x)
-    sed = getattr(layers, model_config.SED)(model_config.SED_ARGS)(x)
-    doa = getattr(layers, model_config.DOA)(model_config.DOA_ARGS)(x)
+    x = getattr(modules, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
+    x = getattr(modules, model_config.SECOND)(model_config.SECOND_ARGS)(x)
+    sed = getattr(modules, model_config.SED)(model_config.SED_ARGS)(x)
+    doa = getattr(modules, model_config.DOA)(model_config.DOA_ARGS)(x)
 
     return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
+
