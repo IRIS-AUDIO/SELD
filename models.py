e3e813495e68250513bd79d87af34daf8c3bcaa7
SongJae <songjae97@naver.com>
2021-03-09 00:40:31 +0000

    resnet model added

--- models.py
+++ models.py
@@ -1,79 +1,165 @@
 import tensorflow as tf
-from tensorflow.keras.activations import *
 from tensorflow.keras.layers import *
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import activations
 
-import layers
+def res_identity(x, filters): 
+ 
+  x_skip = x 
+  f1, f2 = filters
+
+  
+  x = Conv2D(f1, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_regularizer=l2(0.001))(x)
+  x = BatchNormalization()(x)
+  x = Activation(activations.relu)(x)
+
+ 
+  x = Conv2D(f1, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_regularizer=l2(0.001))(x)
+  x = BatchNormalization()(x)
+  x = Activation(activations.relu)(x)
+
+  
+  x = Conv2D(f2, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_regularizer=l2(0.001))(x)
+  x = BatchNormalization()(x)
+  
+  x = Add()([x, x_skip])
+  x = Activation(activations.relu)(x)
+
+  return x
+
+def res_conv(x, s, filters):
+   
+    x_skip = x
+    f1, f2 = filters
+
+    
+    x = Conv2D(f1, kernel_size=(1, 1), strides=(s, s), padding='valid', kernel_regularizer=l2(0.001))(x)
+    
+    x = BatchNormalization()(x)
+    x = Activation(activations.relu)(x)
+
+    
+    x = Conv2D(f1, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_regularizer=l2(0.001))(x)
+    x = BatchNormalization()(x)
+    x = Activation(activations.relu)(x)
+
+    
+    x = Conv2D(f2, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_regularizer=l2(0.001))(x)
+    x = BatchNormalization()(x)
+
+   
+    x_skip = Conv2D(f2, kernel_size=(1, 1), strides=(s, s), padding='valid', kernel_regularizer=l2(0.001))(x_skip)
+    x_skip = BatchNormalization()(x_skip)
+
+    
+    x = Add()([x, x_skip])
+    x = Activation(activations.relu)(x)
+
+    return x
+  
+def resnet50(data_in, spec_start, spec_cnn):
+    
+    #input_im = Input(spec_cnn)
+    spec_cnn = ZeroPadding2D(padding=(3, 3))(spec_cnn)
 
 
-def seldnet(input_shape, model_config):
+    spec_cnn = Conv2D(64, kernel_size=(7, 7), strides=(2, 2))(spec_cnn)
+    spec_cnn = BatchNormalization()(spec_cnn)
+    spec_cnn = Activation(activations.relu)(spec_cnn)
+    spec_cnn = MaxPooling2D((3, 3), strides=(2, 2))(spec_cnn)
+
+
+    spec_cnn = res_conv(spec_cnn, s=1, filters=(64, 256))
+    spec_cnn = res_identity(spec_cnn, filters=(64, 256))
+    spec_cnn = res_identity(spec_cnn, filters=(64, 256))
+
+   
+    spec_cnn = res_conv(spec_cnn, s=2, filters=(128, 512))
+    spec_cnn = res_identity(spec_cnn, filters=(128, 512))
+    spec_cnn = res_identity(spec_cnn, filters=(128, 512))
+    spec_cnn = res_identity(spec_cnn, filters=(128, 512))
+
+
+    spec_cnn = res_conv(spec_cnn, s=2, filters=(256, 1024))
+    spec_cnn = res_identity(spec_cnn, filters=(256, 1024))
+    spec_cnn = res_identity(spec_cnn, filters=(256, 1024))
+    spec_cnn = res_identity(spec_cnn, filters=(256, 1024))
+    spec_cnn = res_identity(spec_cnn, filters=(256, 1024))
+    spec_cnn = res_identity(spec_cnn, filters=(256, 1024))
+
+
+    spec_cnn = res_conv(spec_cnn, s=2, filters=(512, 2048))
+    spec_cnn = res_identity(spec_cnn, filters=(512, 2048))
+    spec_cnn = res_identity(spec_cnn, filters=(512, 2048))
+
+
+    spec_cnn = AveragePooling2D((2, 2), padding='same')(spec_cnn)
+
+    spec_cnn = Flatten()(spec_cnn)
+    spec_cnn = Dense(14, activation='softmax', kernel_initializer='he_normal')(spec_cnn)
+
+    model = tf.keras.Model(inputs=spec_start, outputs=spec_cnn, name='Resnet50')
+    return model
+    
+
+
+def build_seldnet(data_in, 
+                  n_classes=1, 
+                  dropout_rate=0., 
+                  nb_cnn2d_filt=64, 
+                  pool_size=None,
+                  rnn_size=None, 
+                  fnn_size=None):
     '''
     regression SELDnet
-    input_shape: [batch, time, freq, chan]
-    model_config: config from argparse
+    data_in: [batch, freq, time, chan]
     '''
+    if pool_size is None:
+        pool_size = [8, 8, 2]
+    if rnn_size is None:
+        rnn_size = [128, 128]
+    if fnn_size is None:
+        fnn_size = [128]
+
     # model definition
-    spec_start = Input(shape=input_shape[-3:])
-    x = getattr(layers, 'high_level_feature_representation_'+model_config.high_level_feature_representation)(model_config)(spec_start)
+    spec_start = Input(shape=data_in[-3:])
+    spec_cnn = spec_start
 
-    x = getattr(layers, 'temporal_context_representation_'+model_config.temporal_context_representation)(model_config)(x)
+    resnet50_model = resnet50(data_in, spec_start, spec_cnn)
+    resnet50_model.summary()
+    
+    #for i, convCnt in enumerate(pool_size):
+    #   spec_cnn = Conv2D(nb_cnn2d_filt, kernel_size=3, padding='same')(spec_cnn)
+    #    spec_cnn = BatchNormalization()(spec_cnn)
+    #    spec_cnn = Activation('relu')(spec_cnn)
+    #    spec_cnn = MaxPooling2D(pool_size=(pool_size[i], 1))(spec_cnn)
+    #    spec_cnn = Dropout(dropout_rate)(spec_cnn)
 
-    # sed
-    sed = getattr(layers, 'sed_layer_'+model_config.sed_layer)(
-        model_config, model_config.n_classes)(x)
-    sed = sigmoid(sed)
+    # [b, f, t, c] -> [b, t, c]
+    spec_cnn = Permute((2, 1, 3))(spec_cnn)
+    spec_rnn = Reshape((-1, spec_cnn.shape[-2]*spec_cnn.shape[-1]))(spec_cnn)
 
-    # doa
-    doa = getattr(layers, 'doa_layer_'+model_config.doa_layer)(
-        model_config, model_config.n_classes)(x)
-    doa = tanh(doa)
+    for nb_rnn_filt in rnn_size:
+        spec_rnn = Bidirectional(
+            GRU(nb_rnn_filt, activation='tanh', 
+                dropout=dropout_rate, recurrent_dropout=dropout_rate, 
+                return_sequences=True),
+            merge_mode='mul')(spec_rnn)
 
-    return tf.keras.Model(inputs=spec_start, outputs=[sed, doa])
+    
+    # SED
+    sed = spec_rnn
+    for nb_fnn_filt in fnn_size:
+        sed = Dense(nb_fnn_filt)(sed)
+        sed = Dropout(dropout_rate)(sed)
+    sed = Dense(n_classes, activation='sigmoid', name='sed_out')(sed)
 
+    # DOA
+    doa = spec_rnn
+    for nb_fnn_filt in fnn_size:
+        doa = Dense(nb_fnn_filt)(doa)
+        doa = Dropout(dropout_rate)(doa)
+    doa = Dense(n_classes*3, activation='tanh', name='doa_out')(doa) 
 
-def seldnet_v1(input_shape, model_config):
-    '''
-    regression SELDnet
-    input_shape: [batch, time, freq, chan]
-    model_config: config from argparse
-    '''
-    # model definition
-    spec_start = Input(shape=input_shape[-3:])
-    x = getattr(layers, 'high_level_feature_representation_'+model_config.high_level_feature_representation)(model_config)(spec_start)
-
-    x = getattr(layers, 'temporal_context_representation_'+model_config.temporal_context_representation)(model_config)(x)
-
-    # sed
-    sed = getattr(layers, 'sed_layer_'+model_config.sed_layer)(
-        model_config, model_config.n_classes)(x)
-    sed = sigmoid(sed)
-
-    # doa
-    doa = getattr(layers, 'doa_layer_'+model_config.doa_layer)(
-        model_config, model_config.n_classes)(x)
-    doa *= Concatenate()([sed] * 3)
-    doa = tanh(doa) 
-
-    return tf.keras.Model(inputs=spec_start, outputs=[sed, doa])
-
-
-def seldnet_architecture(input_shape, model_config):
-    # interprets model_config to an actual model
-    inputs = Input(shape=input_shape[-3:])
-
-    x = getattr(layers, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
-    x = getattr(layers, model_config.SECOND)(model_config.SECOND_ARGS)(x)
-    sed = getattr(layers, model_config.SED)(model_config.SED_ARGS)(x)
-    doa = getattr(layers, model_config.DOA)(model_config.DOA_ARGS)(x)
-
-    return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
-
-def xception_gru(input_shape, model_config):
-    # interprets model_config to an actual model
-    inputs = Input(shape=input_shape[-3:])
-
-    x = getattr(layers, model_config.FIRST)(model_config.FIRST_ARGS)(inputs)
-    x = getattr(layers, model_config.SECOND)(model_config.SECOND_ARGS)(x)
-    sed = getattr(layers, model_config.SED)(model_config.SED_ARGS)(x)
-    doa = getattr(layers, model_config.DOA)(model_config.DOA_ARGS)(x)
-
-    return tf.keras.Model(inputs=inputs, outputs=[sed, doa])
+    model = tf.keras.Model(inputs=spec_start, outputs=[sed, doa])
+    return model
