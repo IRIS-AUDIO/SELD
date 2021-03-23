35fcf7974ec7adefcabc4c252d7d94a582bf17d3
daniel03c1 <daniel03c195@gmail.com>
2021-03-23 11:51:32 +0000

    remove complexity calculting func from config_sampling

--- config_sampling_test.py
+++ config_sampling_test.py
@@ -56,41 +56,6 @@ class ConfigSamplingTest(tf.test.TestCase):
         c = dict_add(a, b)
         self.assertEqual(c, gt)
 
-    def test_safe_tuple(self):
-        self.assertEqual((1, 1), safe_tuple(1, 2))
-        self.assertEqual((1, 3), safe_tuple((1, 3), 2))
-        with self.assertRaises(ValueError):
-            safe_tuple((1, 2, 3), 2)
-
-    def test_conv2d_complexity(self):
-        self.assertEqual(
-            conv2d_complexity(input_shape=[32, 32, 3],
-                              filters=16,
-                              kernel_size=3,
-                              strides=1),
-            ([32, 32, 16], {'flops': 442384, 'params': 448}))
-
-    def test_norm_complexity(self):
-        self.assertEqual(
-            norm_complexity(input_shape=[32, 32, 3],
-                              center=True,
-                              scale=True),
-            ([32, 32, 3], {'params': 6}))
-
-    def test_pool2d_complexity(self):
-        self.assertEqual(
-            pool2d_complexity(input_shape=[32, 32, 3],
-                               pool_size=3,
-                               strides=(2, 1)),
-            ([16, 32, 3], {}))
-
-    def test_linear_complexity(self):
-        self.assertEqual(
-            linear_complexity(input_shape=[1, 512],
-                              units=1024,
-                              use_bias=True),
-            ([1, 1024], {'flops': 1048576, 'params': 525312}))
-
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
