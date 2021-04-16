65b23693975a6ab5262a23e095ed091034bd4bb1
daniel03c1 <daniel03c195@gmail.com>
2021-04-16 13:17:58 +0000

    remove model complexity from complexities

--- complexity.py
+++ complexity.py
@@ -10,44 +10,6 @@ import copy
 from utils import dict_add
 
 
-'''            model complexities            '''
-def conv_temporal_complexity(model_config, input_shape):
-    filters = model_config.get('filters', 32)
-    first_kernel_size = model_config.get('first_kernel_size', 7)
-    first_pool_size = model_config.get('first_pool_size', [5, 2])
-    n_classes = model_config.get('n_classes', 14)
-
-    shape = input_shape[-3:]
-    total_cx = {}
-    
-    total_cx, shape = conv2d_complexity(shape, filters, first_kernel_size,
-                                        padding='same', prev_cx=total_cx)
-    total_cx, shape = norm_complexity(shape, prev_cx=total_cx)
-    total_cx, shape = pool2d_complexity(shape, first_pool_size, padding='same',
-                                        prev_cx=total_cx)
-
-    blocks = [key for key in model_config.keys()
-              if key.startswith('BLOCK') and not key.endswith('_ARGS')]
-    blocks.sort()
-
-    for block in blocks:
-        cx, shape = globals()[f'{model_config[block]}_complexity'](
-            model_config[f'{block}_ARGS'], shape)
-        total_cx = dict_add(total_cx, cx)
-
-    cx, sed_shape = globals()[f'{model_config["SED"]}_complexity'](
-        model_config['SED_ARGS'], shape)
-    cx, sed_shape = linear_complexity(sed_shape, n_classes, prev_cx=cx)
-    total_cx = dict_add(total_cx, cx)
-
-    cx, doa_shape = globals()[f'{model_config["SED"]}_complexity'](
-        model_config['SED_ARGS'], shape)
-    cx, doa_shape = linear_complexity(doa_shape, 3*n_classes, prev_cx=cx)
-    total_cx = dict_add(total_cx, cx)
-
-    return total_cx, (sed_shape, doa_shape)
-
-
 '''            module complexities            '''
 def simple_conv_block_complexity(model_config, input_shape):
     filters = model_config['filters']
@@ -552,8 +514,7 @@ if __name__ == '__main__':
     input_shape = [300, 64, 7]
 
     print(conv_temporal_complexity(model_config, input_shape))
-    print(multi_head_attention_complexity(
-              [300, 64], 128, 64//128))
+
     '''
     import models
     model = models.conv_temporal(input_shape, model_config)
