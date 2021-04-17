2c2e210ed0e44fbcaf6a7b4cffe65cf869813317
daniel03c1 <daniel03c195@gmail.com>
2021-04-17 11:03:16 +0000

    restore conv_temporal_complexity

--- complexity.py
+++ complexity.py
@@ -10,6 +10,43 @@ import copy
 from utils import dict_add
 
 
+def conv_temporal_complexity(model_config, input_shape):
+     filters = model_config.get('filters', 32)
+     first_kernel_size = model_config.get('first_kernel_size', 7)
+     first_pool_size = model_config.get('first_pool_size', [5, 1])
+     n_classes = model_config.get('n_classes', 14)
+
+     shape = input_shape[-3:]
+     total_cx = {}
+
+     total_cx, shape = conv2d_complexity(shape, filters, first_kernel_size,
+                                         padding='same', prev_cx=total_cx)
+     total_cx, shape = norm_complexity(shape, prev_cx=total_cx)
+     total_cx, shape = pool2d_complexity(shape, first_pool_size, padding='same',
+                                         prev_cx=total_cx)
+
+     blocks = [key for key in model_config.keys()
+               if key.startswith('BLOCK') and not key.endswith('_ARGS')]
+     blocks.sort()
+
+     for block in blocks:
+         cx, shape = globals()[f'{model_config[block]}_complexity'](
+             model_config[f'{block}_ARGS'], shape)
+         total_cx = dict_add(total_cx, cx)
+
+     cx, sed_shape = globals()[f'{model_config["SED"]}_complexity'](
+         model_config['SED_ARGS'], shape)
+     cx, sed_shape = linear_complexity(sed_shape, n_classes, prev_cx=cx)
+     total_cx = dict_add(total_cx, cx)
+
+     cx, doa_shape = globals()[f'{model_config["DOA"]}_complexity'](
+         model_config['DOA_ARGS'], shape)
+     cx, doa_shape = linear_complexity(doa_shape, 3*n_classes, prev_cx=cx)
+     total_cx = dict_add(total_cx, cx)
+
+     return total_cx, (sed_shape, doa_shape)
+
+
 '''            module complexities            '''
 def simple_conv_block_complexity(model_config, input_shape):
     filters = model_config['filters']
@@ -183,6 +220,8 @@ def sepformer_block_complexity(model_config, input_shape):
     # mandatory parameters (for transformer_encoder_block)
     # 'n_head', 'ff_multiplier', 'kernel_size'
     time, freq, chan = input_shape
+    if freq < 2 or freq % 2:
+        raise ValueError('invalid freq')
 
     intra_shape = [time, freq] # [batch*chan, time, freq]
     cx, intra_shape = transformer_encoder_block_complexity(model_config, 
@@ -518,14 +557,13 @@ def force_1d_shape(shape):
 if __name__ == '__main__':
     import json
 
-    model_config = json.load(open('model_config/conv_temp.json', 'rb'))
+    model_config = json.load(open('model_config/seldnet.json', 'rb'))
     input_shape = [300, 64, 7]
 
     print(conv_temporal_complexity(model_config, input_shape))
 
-    '''
+    import tensorflow as tf
     import models
     model = models.conv_temporal(input_shape, model_config)
-    print(model.count_params())
-    '''
+    print(sum([tf.keras.backend.count_params(p) for p in model.trainable_weights]))
 
