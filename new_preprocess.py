41fae7f5a5ce6e9bda97ebce397aab47efb945c1
pjh <pjh_1996@naver.com>
2021-05-05 10:36:37 +0900

    review applied

--- new_preprocess.py
+++ new_preprocess.py
@@ -4,12 +4,6 @@
 import tensorflow_io as tfio
 import tensorflow as tf
 
-def vectorized_mel(spec, sr, mels, fmin=0, fmax=None):
-    if fmax==None:
-        fmax = sr//2
-    mel_spec = tfio.experimental.audio.melscale(spec, sr, mels, fmin, fmax)
-    dbscale_mel_spectrogram = tfio.experimental.audio.dbscale(mel_spec, top_db=80)
-    return dbscale_mel_spectrogram
 
 def foa_intensity_vectors_tf(spectrogram, eps=1e-8):
 
@@ -28,6 +22,7 @@ def foa_intensity_vectors_tf(spectrogram, eps=1e-8):
     # apply mel matrix without db ...
     return tf.stack([IVx, IVy, IVz], axis=0)
 
+
 def gcc_features_tf(complex_specs, n_mels):
     n_chan = complex_specs.shape[0]
     gcc_feat = []
@@ -41,32 +36,31 @@ def gcc_features_tf(complex_specs, n_mels):
 
     return tf.stack(gcc_feat, axis=0)
 
-def get_preprocessed_x_tf(wav, sample_rate, mode='foa', n_mels=64,
+
+def get_preprocessed_x_tf(wav, sr, mode='foa', n_mels=64,
                           multiplier=5, max_label_length=600, win_length=960,
                           hop_length=480, n_fft=1024):
 
-    # audio = tf.convert_to_tensor(wav.cpu().numpy()) 
-    audio_slice = audio[:]
-    audio_tensor = tf.cast(audio_slice, tf.float32) / 32768.0
+    mel_mat = tf.signal.linear_to_mel_weight_matrix(num_mel_bins=n_mels,
+                                                    num_spectrogram_bins=n_fft//2+1,
+                                                    sample_rate=sr,
+                                                    lower_edge_hertz=0,
+                                                    upper_edge_hertz=sr//2)
+
+
+    audio_tensor = tf.cast(audio.to_tensor(), tf.float32) / 32768.0
 
     audio_tensor = tf.transpose(audio_tensor)
-    spectrogram = tf.map_fn(lambda x :tf.signal.stft(x, win_length, hop_length,
-                                                     n_fft, pad_end=True),
-                            elems = audio_tensor, dtype=tf.complex64)
+    spectrogram = tf.signal.stft(audio_tensor, 960, 480, 1024, pad_end=True)
     norm_spec = tf.math.abs(spectrogram)
-    mel_spec = tf.map_fn(lambda x :vectorized_mel(x, sample_rate, n_mels), 
-                                elems = norm_spec)
+    mel_spec = tf.matmul(norm_spec, mel_mat)
+    mel_spec = tfio.experimental.audio.dbscale(mel_spec, top_db=80)
     features = [mel_spec]
-    
         
     if mode == 'foa':
         foa = foa_intensity_vectors_tf(spectrogram)
-        foa = tf.map_fn(lambda x :tfio.experimental.audio.melscale(x,
-                                                                   sample_rate, 
-                                                                   n_mels, 
-                                                                   0, sample_rate//2), 
-                        elems = foa,
-                        dtype = tf.float32)
+        foa = tf.matmul(foa, mel_mat)
+        foa = tfio.experimental.audio.dbscale(foa, top_db=80)
         features.append(foa)
         
     elif mode == 'mic':
@@ -87,7 +81,7 @@ def get_preprocessed_x_tf(wav, sample_rate, mode='foa', n_mels=64,
         features = tf.pad(features, pad, 'constant')
     else:
         features = features[:max_len]
-
+    return features
 
 if __name__ == "__main__":
     audio = tfio.audio.AudioIOTensor('/home/pjh/seld-dcase2020/foa_dev/fold1_room1_mix001_ov1.wav')
