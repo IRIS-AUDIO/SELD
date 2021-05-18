fa7ac2737b1a3b9767fab9df610b3efb9eac3f85
daniel03c1 <daniel03c195@gmail.com>
2021-05-18 11:07:25 +0000

    use kstest for diagnose

--- plot_temp.py
+++ plot_temp.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     }
 
     pairs = []
-    for j in ['vad_7-0_results', 'vad_7-1_results']:
+    for j in ['vad_6-0_results', 'vad_6-1_results']:
         if not j.endswith('.json'):
             j = f'{j}.json'
 
@@ -49,8 +49,6 @@ if __name__ == '__main__':
         recall = pair['perf']['val_recall'][0]
         f1 = 2 * precision * recall / (precision + recall + 1e-8)
         pair['perf']['val_f1score'] = f1
-    pairs = [pair for pair in pairs 
-             if pair['config']['BLOCK1_ARGS']['strides'][-1] == 2]
 
     for pair in pairs:
         outputs['filters0'].append(pair['config']['BLOCK0_ARGS']['filters'])
@@ -69,72 +67,36 @@ if __name__ == '__main__':
             score = score[-1]
         outputs[keyword].append(score)
 
-    xs = np.array([
-        outputs['filters0'],
-        outputs['depth0'],
-        outputs['groups0'],
-        outputs['filters1'],
-        outputs['depth1'],
-        # outputs['strides1'],
-        outputs['groups1'],
-        # outputs['bottleneck_ratio']
-    ], dtype=np.float32)
+    mapper = {0: 'filters0',
+              1: 'depth0',
+              2: 'groups0',
+              3: 'filters1',
+              4: 'depth1',
+              5: 'strides1',
+              6: 'groups1',
+              7: 'bottleneck_ratio'}
+    xs = np.array([outputs[mapper[i]] for i in range(8)],
+                  dtype=np.float32)
     xs = xs.T
     ys = np.array(outputs[keyword], dtype=np.float32)
 
-    import tensorflow as tf
+    from scipy.stats import ks_2samp
+    from itertools import combinations
 
-    class DistLayer(tf.keras.layers.Layer):
-        def __init__(self, num_feats=8):
-            super(DistLayer, self).__init__()
-            self.num_feats = num_feats
+    for i in range(8):
+        print(mapper[i])
+        stats = []
+        unique_values = np.unique(xs[..., i])
+        for value in unique_values:
+            stats.append(ys[xs[..., i] == value])
 
-        def build(self, input_shape):
-            sqrt8 = math.sqrt(8)
+        # comb = list(combinations(range(len(unique_values)), 2))
+        comb = [[j, k] for j in range(len(unique_values))
+                       for k in range(len(unique_values))]
 
-            # for relative
-            self.offset = self.add_variable(
-                'offset', initializer='random_normal', 
-                shape=[1, self.num_feats])
-            self.relations = self.add_variable(
-                'relations', initializer='random_normal', 
-                shape=[1, self.num_feats])
-            self.f_weights = self.add_variable(
-                'f_weights', initializer='random_normal', 
-                shape=[1, self.num_feats])
-            self.r_weight = self.add_variable(
-                'r_weight', initializer='random_uniform', shape=[])
-
-            self.y = self.add_variable(
-                'y', initializer='random_uniform', shape=[])
-
-        def call(self, inputs):
-            gumbel_noise = -tf.math.log(-tf.math.log(tf.random.uniform(tf.shape(inputs)) + 1e-8)+1e-8)
-            f_weights = tf.keras.activations.sigmoid((self.f_weights + gumbel_noise) * 5)
-            rel_dist = tf.keras.losses.cosine_similarity(
-                self.relations * f_weights,
-                (inputs+self.offset) * f_weights)
-            rel_dist = (rel_dist + 1) \
-                     * tf.keras.activations.softplus(self.r_weight)
-
-            return - rel_dist + self.y # 0.92 # 0.9158343
-
-    def generate_model(num_feats=8):
-        inputs = tf.keras.layers.Input(shape=(num_feats,))
-        outputs = DistLayer(num_feats)(inputs)
-        return tf.keras.Model(inputs=inputs, outputs=outputs)
-
-    model = generate_model(xs.shape[-1])
-    model.compile(optimizer=tf.keras.optimizers.SGD(momentum=0.9), loss='mse')
-
-    model.fit(xs, ys, batch_size=100, epochs=4000, shuffle=True, verbose=False)
-
-    print('losses(MSE)', tf.keras.losses.MSE(ys, model(xs)))
-
-    print('offset', model.weights[0].numpy())
-    print('relations:', tf.math.l2_normalize(model.weights[1]).numpy())
-    print('f_weights:', 
-          tf.keras.activations.sigmoid(model.weights[-3] * 5).numpy())
-    print('r_weight', tf.keras.activations.softplus(model.weights[-2]).numpy())
-    print('imaginary y', model.weights[-1].numpy())
+        for j, k in comb:
+            pvalue = ks_2samp(stats[j], stats[k]).pvalue
+            if pvalue < 0.2 and i != k:
+                print(unique_values[j], unique_values[k], pvalue, sep='\t')
+        print()
 
