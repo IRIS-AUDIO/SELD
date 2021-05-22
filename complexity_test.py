ca07d7c33843a8de93fbc1f54ed4b0bbfbcc4a25
daniel03c1 <daniel03c195@gmail.com>
2021-05-22 00:51:17 +0000

    add mother_block_complexity

--- complexity_test.py
+++ complexity_test.py
@@ -9,6 +9,24 @@ class ComplexityTest(tf.test.TestCase):
     def setUp(self):
         self.prev_cx = {'flops': 456, 'params': 123}
 
+    def test_mother_block_complexity(self):
+        model_config = {
+            'filters0': 32,
+            'filters1': 32,
+            'filters2': 0,
+            'kernel_size0': 3,
+            'kernel_size1': 5,
+            'kernel_size2': 0,
+            'connect0': [1],
+            'connect1': [1, 1],
+            'connect2': [0, 0, 1],
+            'strides': (1, 1),
+        }
+        self.complexity_test(mother_block_complexity,
+                             mother_block,
+                             model_config,
+                             [32, 32, 16])
+
     def test_simple_conv_block_complexity(self):
         model_config = {
             'filters': [32, 32],
