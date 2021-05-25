3f362bb2470eac31973fa4f454b0fe89da3c8647
daniel03c1 <daniel03c195@gmail.com>
2021-05-25 10:45:00 +0000

    add simple_mask
    current version of mask raises error

--- transforms.py
+++ transforms.py
@@ -3,9 +3,7 @@ from numpy import pi
 from utils import safe_div
 
 
-def mask(specs, axis, max_mask_size=None, period=None, n_mask=1):
-    if period is None:
-        period = tf.shape(specs)[axis]
+def mask(specs, axis, max_mask_size=None, period=100, n_mask=1):
     total = tf.shape(specs[:period])[axis]
     if max_mask_size is None:
         max_mask_size = total
@@ -43,6 +41,38 @@ def mask(specs, axis, max_mask_size=None, period=None, n_mask=1):
     specs = tf.map_fn(_mask, specs)
     specs = tf.reshape(specs, shape)
     return specs
+
+
+def simple_mask(specs, axis, max_mask_size=None, n_mask=1):
+     def make_shape(size):
+         # returns (1, ..., size, ..., 1)
+         shape = [1] * len(specs.shape)
+         shape[axis] = size
+         return tuple(shape)
+
+     total = tf.shape(specs)[axis]
+     mask = tf.ones(make_shape(total), dtype=specs.dtype)
+
+     if max_mask_size is None:
+         max_mask_size = total
+
+     def apply_random_mask(mask):
+         size = tf.random.uniform([], maxval=max_mask_size, dtype=tf.int32)
+         offset = tf.random.uniform([], maxval=total-size, dtype=tf.int32)
+
+         mask *= tf.concat(
+             (tf.ones(shape=make_shape(offset), dtype=mask.dtype),
+              tf.zeros(shape=make_shape(size), dtype=mask.dtype),
+              tf.ones(shape=make_shape(total-size-offset), dtype=mask.dtype)),
+             axis=axis)
+         return mask
+
+     i = tf.constant(0)
+     cond = lambda i, m: i < n_mask
+     body = lambda i, m: (i+1, apply_random_mask(m))
+     _, mask = tf.while_loop(cond, body, (i, mask))
+
+     return specs * mask
 
 
 def foa_intensity_vec_aug(x, y):
