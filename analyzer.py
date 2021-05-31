bb81eaea43145927653ff181177671d73589bca6
daniel03c1 <daniel03c195@gmail.com>
2021-05-31 11:17:30 +0000

    add visualize, var_of_interest options to analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import matplotlib.pyplot as plt
 import numpy as np
 from itertools import combinations
 from scipy.stats import ks_2samp
@@ -9,11 +10,14 @@ args = argparse.ArgumentParser()
 args.add_argument('--results', type=str, 
                   default='vad_6-0_results,vad_6-1_results')
 args.add_argument('--keyword', type=str, default='val_auc')
+args.add_argument('--keyword2', type=str, default='test_auc')
 args.add_argument('--n_stages', type=int, default=3)
 args.add_argument('--count1d', action='store_true')
 args.add_argument('--stagewise', action='store_true')
 args.add_argument('--stagewise_exist', action='store_true')
+args.add_argument('--var_of_interest', type=str, default='')
 args.add_argument('--verbose', action='store_true')
+args.add_argument('--visualize', action='store_true')
 args.add_argument('--black_list', type=str, default='')
 args.add_argument('--a', type=float, default=0.05)
 args.add_argument('--min_samples', type=int, default=1)
@@ -119,6 +123,7 @@ def get_ks_test_values(values, perfs, min_samples=1, a=0.05, verbose=False):
 if __name__ == '__main__':
     config = args.parse_args()
     keyword = config.keyword
+    keyword2 = config.keyword2
     pairs = []
 
     # 1. load results
@@ -131,9 +136,6 @@ if __name__ == '__main__':
 
         for key in results.keys():
             if key.isdigit():
-                pairs.append(results[key])
-
-                # temp
                 for i in range(config.n_stages):
                     c = results[key]['config']
                     if c[f'BLOCK{i}'] == 'mother_stage':
@@ -165,6 +167,14 @@ if __name__ == '__main__':
                             c_args['depth'] = 1
                         c[f'BLOCK{i}_ARGS']['n_convs'] = n_convs
 
+                        # close connection
+                        c[f'BLOCK{i}_ARGS']['close_connect'] = 0
+                        for j in range(3):
+                            if c_args[f'filters{j}'] > 0 and c_args[f'connect{j}'][-1] == 0:
+                                c[f'BLOCK{i}_ARGS']['close_connect'] += 1 / n_convs
+
+                pairs.append(results[key])
+
     # 1.1 black list
     for stage in config.black_list.split(','):
         pairs = filter_fn(
@@ -193,6 +203,7 @@ if __name__ == '__main__':
     # 2.2 make table
     table = {feat: [] for feat in feats}
     table[keyword] = []
+    table[keyword2] = []
 
     for pair in pairs:
         for feat in feats.keys():
@@ -207,10 +218,11 @@ if __name__ == '__main__':
 
             table[feat].append(value)
 
-        k_value = pair['perf'][keyword]
-        if isinstance(k_value, list):
-            k_value = k_value[-1]
-        table[keyword].append(k_value)
+        for kw in [keyword, keyword2]:
+            k_value = pair['perf'][kw]
+            if isinstance(k_value, list):
+                k_value = k_value[-1]
+            table[kw].append(k_value)
 
     # 2.3 add features
     if config.count1d:
@@ -242,30 +254,44 @@ if __name__ == '__main__':
 
     # 3.1 find significant variables
     for rv in table.keys():
-        if rv == keyword:
+        if rv in [keyword, keyword2]:
+            continue
+
+        if len(config.var_of_interest) > 0 and rv != config.var_of_interest:
             continue
 
         unique_values = sorted(np.unique(table[rv]))
         if len(unique_values) == 1:
             continue
 
-        print(f'{rv}')
-        perfs = [table[keyword][table[rv] == value]
-                 for value in unique_values]
-        pvalues = get_ks_test_values(
-            unique_values, perfs, 
-            min_samples=config.min_samples, a=config.a, verbose=config.verbose)
-        n_samples = [len(p) for p in perfs]
+        if config.visualize:
+            for value in unique_values:
+                mask = table[rv] == value
+                plt.plot(table[keyword][mask], 
+                         table[keyword2][mask], '.', label=value)
+            plt.title(rv)
+            plt.xlabel(keyword)
+            plt.ylabel(keyword2)
+            plt.legend()
+            plt.show()
+        else:
+            print(f'{rv}')
+            perfs = [table[keyword][table[rv] == value]
+                     for value in unique_values]
+            pvalues = get_ks_test_values(
+                unique_values, perfs, 
+                min_samples=config.min_samples, a=config.a, verbose=config.verbose)
+            n_samples = [len(p) for p in perfs]
 
-        for i, pv in enumerate(pvalues):
-            if len(pv) > 0:
-                print(f'{unique_values[i]}: '
-                      f'[{min(pv):.5f}, {max(pv):.5f}] '
-                      f'({np.mean(pv):.5f}) '
-                      f'n_samples={len(perfs[i])}, '
-                      f'{keyword}(min={np.min(perfs[i]):.5f}, '
-                      f'mean={np.mean(perfs[i]):.5f}, '
-                      f'median={np.median(perfs[i]):.5f}, '
-                      f'max={np.max(perfs[i]):.5f})')
-        print()
+            for i, pv in enumerate(pvalues):
+                if len(pv) > 0:
+                    print(f'{unique_values[i]}: '
+                          f'[{min(pv):.5f}, {max(pv):.5f}] '
+                          f'({np.mean(pv):.5f}) '
+                          f'n_samples={len(perfs[i])}, '
+                          f'{keyword}(min={np.min(perfs[i]):.5f}, '
+                          f'mean={np.mean(perfs[i]):.5f}, '
+                          f'median={np.median(perfs[i]):.5f}, '
+                          f'max={np.max(perfs[i]):.5f})')
+            print()
 
