d8eb854931c3fc06cd445b718e262cc278245e13
daniel03c1 <daniel03c195@gmail.com>
2021-06-12 14:50:58 +0000

    rm sqrt from MMSE

--- losses.py
+++ losses.py
@@ -9,18 +9,17 @@ def MMSE(y_true, y_pred):
 
     sed = tf.concat([sed] * 3, axis=-1)
 
-    return tf.keras.backend.sqrt(
-        tf.keras.backend.sum(tf.keras.backend.square(y_true - y_pred) * sed)) \
-                / tf.keras.backend.sum(sed)
+    return tf.keras.backend.sum(tf.keras.backend.square(y_true - y_pred) * sed) \
+            / tf.keras.backend.sum(sed)
 
 
-def focal_loss(y_true, y_pred, alpha, gamma):
+def focal_loss(y_true, y_pred, alpha=0.25, gamma=2):
     eps = 1e-7
     y_pred = tf.clip_by_value(y_pred, eps, 1-eps)
     focal = - y_true * alpha * tf.pow(1 - y_pred, gamma) * tf.math.log(y_pred)\
             - (1-y_true) * alpha * tf.pow(y_pred, gamma) * tf.math.log(1-y_pred)
-
     return tf.reduce_mean(focal)
+
 
 class Focal_Loss:
     def __init__(self, alpha=0.25, gamma=2):
@@ -32,6 +31,5 @@ class Focal_Loss:
         y_pred = tf.clip_by_value(y_pred, eps, 1-eps)
         focal = - y_true * self.alpha * tf.pow(1 - y_pred, self.gamma) * tf.math.log(y_pred)\
                 - (1-y_true) * self.alpha * tf.pow(y_pred, self.gamma) * tf.math.log(1-y_pred)
-
         return tf.reduce_mean(focal)
-        
+ 
