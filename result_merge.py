a4133131ecba35026f9732cdb40c1ce7e16d102d
Seungjin Lee <lsg0385@naver.com>
2021-06-26 16:22:28 +0000

    [fix] bug fix

--- result_merge.py
+++ result_merge.py
@@ -11,7 +11,6 @@ def main():
     config = args.parse_args()
     name = config.name
     paths = sorted(glob(f'./{name}*'))
-
     merged_json = {}
     for idx, path in enumerate(paths):
         with open(path, 'r') as f:
@@ -19,9 +18,10 @@ def main():
         if idx == 0:
             merged_json = tmp
         else:
+            length = len(merged_json)
             for key, val in tmp.items():
                 if key != 'train_config':
-                    merged_json[f'{int(key) + len(merged_json):03}'] = val
+                    merged_json[f'{int(key) + length - 1:03}'] = val
 
     with open(f'merged_{name}.json', 'w') as f:
         json.dump(merged_json, f, indent=4)
